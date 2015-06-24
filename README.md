85ee9dbcea902803ab8a19d582e03a04cd57cd7f
Dmitry Borody <dmitriy.borodiy@gmail.com>
2015-06-24 13:45:38 +0300

    Initial version + updated README

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# redis-lock-cli
-Command line interface for python-redis-lock project. Might be useful when using locks in bash scripts
+# Relock - CLI for Redis locks
+Command line interface for python-redis-lock project. Might be useful when using locks in bash scripts.
+
+For predictable behaviour you should automatically generate lock names so that each lock name is used only once in 
+lock/try-release call sequence.
+
+# Installation
+```sh
+pip install -r ./requirements.txt
+```
+
+# Usage
+## Acquiring a named lock (blocking mode):
+```sh
+relock.py lock <lock_name>
+```
+
+This will block until the lock is acquired.
+
+## Trying to acquire a lock in non-blocking mode:
+
+```sh
+relock.py try <lock_name>
+```
+
+## Releasing a lock:
+```sh
+relock.py release <lock_name>
+```
+
---
+++ relock.py
@@ -0,0 +1,59 @@
+__author__ = 'dmitru'
+
+from sys import argv
+
+import redis, redis_lock
+
+class RedisLockClient:
+    LOCK_ID = 'relock'
+
+    def __init__(self, **kwargs):
+        self.redis = redis.StrictRedis(**kwargs)
+
+    def do_lock(self, lock_name):
+        lock = redis_lock.Lock(self.redis, lock_name, id=RedisLockClient.LOCK_ID)
+        lock.acquire(blocking=True)
+        return 'OK'
+
+    def do_try(self, lock_name):
+        lock = redis_lock.Lock(self.redis, lock_name, id=RedisLockClient.LOCK_ID)
+        if lock.acquire(blocking=False):
+            return 'OK'
+        else:
+            return 'FAIL'
+
+    def do_release(self, lock_name):
+        lock = redis_lock.Lock(self.redis, lock_name, id=RedisLockClient.LOCK_ID)
+        lock.release(force=True)
+        return 'OK'
+
+
+def print_usage():
+    print('Usage: relock.py <lock|try|release> <name of the lock>')
+
+if __name__ == '__main__':
+    if len(argv) < 3:
+        print_usage()
+        exit(1)
+
+    command, lock_name = argv[1], argv[2]
+
+    redis_client = RedisLockClient()
+    try:
+        if command == 'lock':
+            result_code = redis_client.do_lock(lock_name)
+        elif command == 'release':
+            result_code = redis_client.do_release(lock_name)
+        elif command == 'try':
+            result_code = redis_client.do_try(lock_name)
+        else:
+            print('invalid command: %s' % command)
+            print_usage()
+            exit(1)
+
+        print(result_code)
+        exit(0)
+    except Exception as e:
+        print('FAIL\n%s' % e)
+        exit(1)
+
